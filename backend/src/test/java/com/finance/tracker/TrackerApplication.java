5d63b25eda1c593ffb46e756411a12cde9ebf2d2
Gattu Raghavendra <139197775+raghavendra-54@users.noreply.github.com>
2025-08-10 11:29:40 +0530

    Update TrackerApplication.java

--- backend/src/test/java/com/finance/tracker/TrackerApplication.java
+++ backend/src/test/java/com/finance/tracker/TrackerApplication.java
@@ -1,34 +1,10 @@
 package com.finance.tracker;
-
-import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.context.annotation.Bean;
-
 
 @SpringBootApplication
 public class TrackerApplication {
-
-    private static Server h2Server;
-
     public static void main(String[] args) {
-        startH2Server();
-        
         SpringApplication.run(TrackerApplication.class, args);
     }
-
-    private static void startH2Server() {
-            h2Server = Server.createTcpServer(
-                "-tcp",
-                "-tcpAllowOthers",
-                "-tcpPort", "8080",
-                "-baseDir", "./data", 
-                "-ifNotExists"    
-            ).start();
-    @GetMapping("/health")
-    public String healthCheck() {
-        return "OK";
-    }
-    }
+}
